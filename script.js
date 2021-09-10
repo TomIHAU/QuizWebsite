11231ffa2609ffa6b8a37e10ebb89f66c47d328f
TauH <thaywood988@gmail.com>
2021-09-10 11:31:15 +1000

    started html, added queryselectors in js

---
+++ script.js
@@ -0,0 +1,14 @@
+var timer = document.querySelector(".timer");
+var question = document.querySelector(".question");
+var startBtn = document.querySelector(".startBtn");
+var questionList = [];
+var answerList = [[],[]];
+
+
+
+function start(
+    timer = 30;
+    
+);
+
+startBtn.addEventListener("click", start());
