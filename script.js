c2bd3c4bf28c43d91e9ef9db4a732485ac23178a
TauH <thaywood988@gmail.com>
2021-09-10 15:13:40 +1000

    added timer, some functions

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 var timer = document.querySelector(".timer");
 var question = document.querySelector(".question");
 var startBtn = document.querySelector(".startBtn");
+
 var questionList = [];
 var answerList = [[],[]];
+var score;
+var highScore;
+var newName;
+var OldName;
 
+var timerCounter;
+var timerRun;
 
+function win(){
+  localStorage.setItem("highScore", highScore)
+}
 
-function start(
-    timer = 30;
+function startTimer() {
+    timerRun = setInterval(function() {
+      timerCounter--;
+      timer.textContent = timerCounter;
+      if (timerCount === 0) {
+        clearInterval(timerRun);
+      }
+    }, 1000);
+};
+
+function checkHighScore(){
+  if(score > highScore){
+    OldName = newName;
+    highScore = score;
     
-);
+  }
+};
 
-startBtn.addEventListener("click", start());
+function start(){
+    timerCounter = 30;
+
+
+    startTimer();
+};
+
+startBtn.addEventListener("click", start)
